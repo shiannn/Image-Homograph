e4f055b0b0c69bdf9737302131df90647b205270
shiannn <b05502087@ntu.edu.tw>
2022-03-20 00:30:09 +0800

    succeed at homography transformation

--- 2.py
+++ 2.py
@@ -18,6 +18,42 @@ def argparse2():
     
     return args
 
+def bilinear_interpolate(img, H, target_size=None):
+    def get_valid_region(img, grid):
+        #print(grid)
+        is_valid = (
+            (grid[:,:,0] < img.shape[1]) & \
+            (grid[:,:,0] >= 0) & \
+            (grid[:,:,1] < img.shape[0]) & \
+            (grid[:,:,1] >= 0)
+        )
+        return is_valid
+    backward_H = np.linalg.inv(H)
+    u = np.arange(0,img.shape[1])
+    v = np.arange(0,img.shape[0])
+    uu, vv = np.meshgrid(u, v)
+    grid = np.stack([
+        vv,uu,
+        np.ones_like(uu),
+    ],axis=2)
+    #grid_computed = grid.copy()
+    grid[:,:,[0,1]] = grid[:,:,[1,0]]
+    new_grid = np.matmul(backward_H, grid.reshape(-1,3).T).T
+    new_grid = new_grid.reshape(-1, grid.shape[1], 3)
+    
+    #new_grid = np.matmul(backward_H, grid.reshape(-1,3).T).T
+    #new_grid = new_grid.reshape(-1,grid.shape[1],3)
+    #new_grid[:,:,[0,1]] = new_grid[:,:,[1,0]]
+    new_grid = new_grid / new_grid[:,:,2:]
+    
+    new_grid_uv = new_grid[:,:,:2] ### u, v
+    valid_region = get_valid_region(img, np.floor(new_grid_uv).astype(int))
+    new_grid_uv[~valid_region] = 0
+    new_grid_uv = new_grid_uv.astype(int)
+    ret_img = img[(new_grid_uv[:,:,1],new_grid_uv[:,:,0])]
+    
+    return ret_img
+
 def main(args):
     img = cv2.imread(args.input_img)
     print(img.shape)
@@ -34,7 +70,8 @@ def main(args):
         show_img = cv2.circle(show_img, (u,v), radius=5, color=(0, 0, 255), thickness=-1)
     vis_img(cv2.cvtColor(show_img,cv2.COLOR_BGR2RGB))
     H = normalized_DLT(points1, points2, k=points1.shape[0])
-    recons = cv2.warpPerspective(img, H, dsize=(img.shape[1], img.shape[0]))
+    recons = bilinear_interpolate(img, H, target_size=None)
+    #recons = cv2.warpPerspective(img, H, dsize=(img.shape[1], img.shape[0]))
     print(recons.shape)
     vis_img(cv2.cvtColor(recons,cv2.COLOR_BGR2RGB))
 if __name__ == '__main__':
